222804c1997a015519f9ab41da012d98a5375ecc
Dmitriy2024 <Dmitriy2024>
2023-08-07 16:46:52 +0300

    Z32

--- DZ_6/main.py
+++ DZ_6/main.py
@@ -6,3 +6,15 @@ difference = int (input('Введите разность '))
 numberElements = int (input('Введите количество элементов '))
 for i in range(numberElements):
     print(firstElement + i * difference, end=' ' )
+
+
+#Задача 32: Определить индексы элементов массива (списка), значения которых принадлежат 
+# заданному диапазону (т.е. не меньше заданного минимума и не больше заданного максимума)
+# list_1 = [-5, 9, 0, 3, -1, -2, 1, 4, -2, 10, 2, 0, -9, 8, 10, -9, 0, -5, -5, 7]
+# minRange = 5
+# maxRange = 15
+
+
+# for i in range(len(list_1)):
+#     if minRange <= list_1[i] <= maxRange:
+#         print(i, end=' ')
